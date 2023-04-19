faa3e63823ec8253cefea2f65b1f2b08dfe3e912
Matheus Bento <matheus.th.bento@gmail.com>
2023-04-19 11:00:03 -0300

    Updating the repo URL on the generated message footnote

--- src/PeekLinkBot/PeekLinkBotService.cs
+++ src/PeekLinkBot/PeekLinkBotService.cs
@@ -100,7 +100,7 @@ namespace PeekLinkBot
                                 "^(beep bop I'm /u/peek-link-bot, your friendly bot " +
                                 "that checks links beforehand so you don't get bamboozled. " +
                                 "Help me to improve by contributing on) " +
-                                "^[github](https://github.com/matheus-bento/peek-link-bot-v2) ^:)",
+                                "^[github](https://github.com/matheus-bento/peek-link-bot.net) ^:)",
                                 String.Join('\n', linksInfo));
 
                             await redditApi.PostComment(message.Name, reply);
