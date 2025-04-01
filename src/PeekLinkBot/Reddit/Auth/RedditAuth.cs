d986d0730e8b666dd7a14bd42ecc9779a21e856c
Matheus Bento <matheus.th.bento@gmail.com>
2025-04-01 14:22:49 -0300

    Refactoring RedditAuth class to reduce the HttpClient lifespan

--- src/PeekLinkBot/Reddit/Auth/RedditAuth.cs
+++ src/PeekLinkBot/Reddit/Auth/RedditAuth.cs
@@ -18,11 +18,13 @@ namespace PeekLinkBot.Reddit.Auth
     /// </summary>
     public class RedditAuth
     {
+        private readonly IHttpClientFactory _httpClientFactory;
         private readonly ILogger<PeekLinkBotService> _logger;
-        private readonly HttpClient _redditAuthHttpClient;
 
         private readonly string _username;
         private readonly string _password;
+        private readonly string _clientID;
+        private readonly string _secret;
 
         public RedditAuth(
             IHttpClientFactory httpClientFactory,
@@ -32,18 +34,27 @@ namespace PeekLinkBot.Reddit.Auth
             string clientID,
             string secret)
         {
+            this._httpClientFactory = httpClientFactory;
             this._logger = logger;
-
-            this._redditAuthHttpClient = httpClientFactory.CreateClient("RedditAuth");
-
-            // Sets the Authorization header according to reddit's specification.
-            // Reference: https://github.com/reddit-archive/reddit/wiki/OAuth2#retrieving-the-access-token
-            byte[] credentialsBytes = Encoding.ASCII.GetBytes(clientID + ":" + secret);
-            this._redditAuthHttpClient.DefaultRequestHeaders.Authorization =
-                new AuthenticationHeaderValue("Basic", Convert.ToBase64String(credentialsBytes));
 
             this._username = username;
             this._password = password;
+            this._clientID = clientID;
+            this._secret = secret;
+        }
+
+        private HttpClient GetHttpClient()
+        {
+            HttpClient httpClient = this._httpClientFactory.CreateClient("RedditAuth");
+
+            // Sets the Authorization header according to reddit's specification.
+            // Reference: https://github.com/reddit-archive/reddit/wiki/OAuth2#retrieving-the-access-token
+            byte[] credentialsBytes = Encoding.ASCII.GetBytes(this._clientID + ":" + this._secret);
+
+            httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Basic", Convert.ToBase64String(credentialsBytes));
+
+            return httpClient;
         }
 
         /// <summary>
@@ -51,40 +62,43 @@ namespace PeekLinkBot.Reddit.Auth
         /// </summary>
         public async Task<AccessTokenResponseContent> GetAccessToken()
         {
-            HttpResponseMessage response =
-                await this._redditAuthHttpClient.PostAsync(
-                    "/api/v1/access_token",
-                    new FormUrlEncodedContent(
-                        new Dictionary<string, string>
-                        {
+            using (HttpClient httpClient = this.GetHttpClient())
+            {
+                HttpResponseMessage response =
+                    await httpClient.PostAsync(
+                        "/api/v1/access_token",
+                        new FormUrlEncodedContent(
+                            new Dictionary<string, string>
+                            {
                             { "grant_type", "password" },
                             { "username", this._username },
                             { "password", this._password }
-                        }
-                    )
-                );
+                            }
+                        )
+                    );
 
-            this._logger.LogDebug("Request: {0}", response.RequestMessage);
-            this._logger.LogDebug("Request Content: {0}", await response.RequestMessage.Content.ReadAsStringAsync());
+                this._logger.LogDebug("Request: {0}", response.RequestMessage);
+                this._logger.LogDebug("Request Content: {0}", await response.RequestMessage.Content.ReadAsStringAsync());
 
-            this._logger.LogDebug("Response: {0}", response);
-            this._logger.LogDebug("Response Content: {0}", await response.Content.ReadAsStringAsync());
+                this._logger.LogDebug("Response: {0}", response);
+                this._logger.LogDebug("Response Content: {0}", await response.Content.ReadAsStringAsync());
 
-            response.EnsureSuccessStatusCode();
+                response.EnsureSuccessStatusCode();
 
-            AccessTokenResponseContent content = 
-                JsonConvert.DeserializeObject<AccessTokenResponseContent>(
-                    await response.Content.ReadAsStringAsync(),
-                    new JsonSerializerSettings
-                    {
-                        ContractResolver = new DefaultContractResolver
+                AccessTokenResponseContent content = 
+                    JsonConvert.DeserializeObject<AccessTokenResponseContent>(
+                        await response.Content.ReadAsStringAsync(),
+                        new JsonSerializerSettings
                         {
-                            NamingStrategy = new SnakeCaseNamingStrategy()
+                            ContractResolver = new DefaultContractResolver
+                            {
+                                NamingStrategy = new SnakeCaseNamingStrategy()
+                            }
                         }
-                    }
-                );
+                    );
 
-            return content;
+                return content;
+            }
         }
     }
 }
