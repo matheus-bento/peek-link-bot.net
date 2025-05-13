0c1b36dd06945355809b8d9a5077516815af300f
Matheus Bento <matheus.th.bento@gmail.com>
2025-05-13 12:36:18 -0300

    Fixing the PeekLinkBotService to call GetAccessToken only after the current token expires

---
+++ src/PeekLinkBot/Reddit/Model/AccessTokenData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PeekLinkBot.Reddit.Model
+{
+    public class AccessTokenData
+    {
+        public string AccessToken { get; set; }
+        public DateTime UtcExpirationDate { get; set; }
+    }
+}
