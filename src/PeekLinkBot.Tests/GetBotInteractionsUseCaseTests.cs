627af119f73fd1f3df984202f51c173ec628e9d3
Matheus Bento <matheus.th.bento@gmail.com>
2025-05-15 21:03:44 -0300

    Tests for the bot interaction use cases

---
+++ src/PeekLinkBot.Tests/GetBotInteractionsUseCaseTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeekLinkBot.Data.Repositories;
+using PeekLinkBot.Domain.Dto;
+using PeekLinkBot.Domain.UseCases;
+using PeekLinkBot.Tests.Core;
+
+namespace PeekLinkBot.Tests
+{
+    [TestClass]
+    public class GetBotInteractionsUseCaseTests
+    {
+        private IBotInteractionRepository _repository;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this._repository = new MockBotInteractionRepository();
+        }
+
+        [TestMethod]
+        [TestCategory("GetBotInteractionsUseCase")]
+        public async Task ShouldNotReturnNullWhenNoInteractions()
+        {
+            var getBotInteractionsUseCase =
+                new GetBotInteractionsUseCase(this._repository);
+
+            IEnumerable<BotInteractionDto> botInteractions =
+                await getBotInteractionsUseCase.Execute();
+
+            Assert.IsNotNull(botInteractions);
+        }
+
+        [TestMethod]
+        [TestCategory("GetBotInteractionsUseCase")]
+        public async Task ShouldReturnEmptyListWhenNoInteractions()
+        {
+            var getBotInteractionsUseCase =
+                new GetBotInteractionsUseCase(this._repository);
+
+            IEnumerable<BotInteractionDto> botInteractions =
+                await getBotInteractionsUseCase.Execute();
+
+            Assert.IsEmpty(botInteractions);
+        }
+
+
+        [TestMethod]
+        [TestCategory("GetBotInteractionsUseCase")]
+        public async Task ShouldReturnStoredInteractions()
+        {
+            var saveInteractionUseCase =
+                new SaveInteractionUseCase(this._repository);
+
+            var creationDate = DateTime.UtcNow;
+
+            await saveInteractionUseCase.Execute(new BotInteractionDto
+            {
+                OriginalComment = new CommentDto
+                {
+                    RedditId = "ogCommentId",
+                    Username = "ogUsername",
+                    CreatedAtUtc = creationDate,
+                    Content = "ogContent"
+                },
+                TriggerComment = new CommentDto
+                {
+                    RedditId = "triggerId",
+                    Username = "triggerUsername",
+                    CreatedAtUtc = creationDate,
+                    Content = "triggerContent"
+                },
+                ReplyComment = new CommentDto
+                {
+                    RedditId = "replyId",
+                    Username = "replyUsername",
+                    CreatedAtUtc = creationDate,
+                    Content = "replyContent"
+                }
+            });
+
+            var getBotInteractionsUseCase =
+                new GetBotInteractionsUseCase(this._repository);
+
+            IEnumerable<BotInteractionDto> botInteractions =
+                await getBotInteractionsUseCase.Execute();
+
+            Assert.ContainsSingle(botInteractions);
+
+            BotInteractionDto botInteraction = botInteractions.FirstOrDefault();
+
+            Assert.IsNotNull(botInteraction.Id);
+
+            Assert.AreEqual("ogCommentId", botInteraction.OriginalComment.RedditId);
+            Assert.AreEqual("ogUsername", botInteraction.OriginalComment.Username);
+            Assert.AreEqual(creationDate, botInteraction.OriginalComment.CreatedAtUtc);
+            Assert.AreEqual("ogContent", botInteraction.OriginalComment.Content);
+
+            Assert.AreEqual("triggerId", botInteraction.TriggerComment.RedditId);
+            Assert.AreEqual("triggerUsername", botInteraction.TriggerComment.Username);
+            Assert.AreEqual(creationDate, botInteraction.TriggerComment.CreatedAtUtc);
+            Assert.AreEqual("triggerContent", botInteraction.TriggerComment.Content);
+
+            Assert.AreEqual("replyId", botInteraction.ReplyComment.RedditId);
+            Assert.AreEqual("replyUsername", botInteraction.ReplyComment.Username);
+            Assert.AreEqual(creationDate, botInteraction.ReplyComment.CreatedAtUtc);
+            Assert.AreEqual("replyContent", botInteraction.ReplyComment.Content);
+        }
+    }
+}
---
+++ src/PeekLinkBot.Tests/SaveInteractionUseCaseTests.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeekLinkBot.Data.Repositories;
+using PeekLinkBot.Domain.Dto;
+using PeekLinkBot.Domain.Exceptions;
+using PeekLinkBot.Domain.UseCases;
+using PeekLinkBot.Tests.Core;
+
+namespace PeekLinkBot.Tests
+{
+    [TestClass]
+    public class SaveInteractionUseCaseTests
+    {
+        private IBotInteractionRepository _repository;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this._repository = new MockBotInteractionRepository();
+        }
+
+        [TestMethod]
+        [TestCategory("SaveInteractionUseCase")]
+        public async Task ShouldNotSaveWithNullArgument()
+        {
+            var saveInteractionUseCase =
+                new SaveInteractionUseCase(this._repository);
+
+            await Assert.ThrowsExactlyAsync<ArgumentNullException>(
+                () => saveInteractionUseCase.Execute(null));
+        }
+
+        [TestMethod]
+        [TestCategory("SaveInteractionUseCase")]
+        public async Task ShouldNotSaveWithoutOriginalComment()
+        {
+            var saveInteractionUseCase =
+                new SaveInteractionUseCase(this._repository);
+
+            var botInteraction = new BotInteractionDto
+            {
+                OriginalComment = null,
+                TriggerComment = new CommentDto
+                {
+                    RedditId = "triggerId",
+                    Username = "triggerUsername",
+                    CreatedAtUtc = DateTime.UtcNow,
+                    Content = "triggerContent"
+                },
+                ReplyComment = new CommentDto
+                {
+                    RedditId = "replyId",
+                    Username = "replyUsername",
+                    CreatedAtUtc = DateTime.UtcNow,
+                    Content = "replyContent"
+                }
+            };
+
+            await Assert.ThrowsExactlyAsync<CommentNotInformedException>(
+                () => saveInteractionUseCase.Execute(botInteraction));
+        }
+
+        [TestMethod]
+        [TestCategory("SaveInteractionUseCase")]
+        public async Task ShouldNotSaveWithoutTriggerComment()
+        {
+            var saveInteractionUseCase =
+                new SaveInteractionUseCase(this._repository);
+
+            var botInteraction = new BotInteractionDto
+            {
+                OriginalComment = new CommentDto
+                {
+                    RedditId = "ogCommentId",
+                    Username = "ogUsername",
+                    CreatedAtUtc = DateTime.UtcNow,
+                    Content = "ogContent"
+                },
+                TriggerComment = null,
+                ReplyComment = new CommentDto
+                {
+                    RedditId = "replyId",
+                    Username = "replyUsername",
+                    CreatedAtUtc = DateTime.UtcNow,
+                    Content = "replyContent"
+                }
+            };
+
+            await Assert.ThrowsExactlyAsync<CommentNotInformedException>(
+                () => saveInteractionUseCase.Execute(botInteraction));
+        }
+
+        [TestMethod]
+        [TestCategory("SaveInteractionUseCase")]
+        public async Task ShouldNotSaveWithoutReplyComment()
+        {
+            var saveInteractionUseCase =
+                new SaveInteractionUseCase(this._repository);
+
+            var botInteraction = new BotInteractionDto
+            {
+                OriginalComment = new CommentDto
+                {
+                    RedditId = "ogCommentId",
+                    Username = "ogUsername",
+                    CreatedAtUtc = DateTime.UtcNow,
+                    Content = "ogContent"
+                },
+                TriggerComment = new CommentDto
+                {
+                    RedditId = "triggerId",
+                    Username = "triggerUsername",
+                    CreatedAtUtc = DateTime.UtcNow,
+                    Content = "triggerContent"
+                },
+                ReplyComment = null
+            };
+
+            await Assert.ThrowsExactlyAsync<CommentNotInformedException>(
+                () => saveInteractionUseCase.Execute(botInteraction));
+        }
+
+        [TestMethod]
+        [TestCategory("SaveInteractionUseCase")]
+        public async Task ShouldSaveInteraction()
+        {
+            var saveInteractionUseCase =
+                new SaveInteractionUseCase(this._repository);
+
+            var botInteraction = new BotInteractionDto
+            {
+                OriginalComment = new CommentDto
+                {
+                    RedditId = "ogCommentId",
+                    Username = "ogUsername",
+                    CreatedAtUtc = DateTime.UtcNow,
+                    Content = "ogContent"
+                },
+                TriggerComment = new CommentDto
+                {
+                    RedditId = "triggerId",
+                    Username = "triggerUsername",
+                    CreatedAtUtc = DateTime.UtcNow,
+                    Content = "triggerContent"
+                },
+                ReplyComment = new CommentDto
+                {
+                    RedditId = "replyId",
+                    Username = "replyUsername",
+                    CreatedAtUtc = DateTime.UtcNow,
+                    Content = "replyContent"
+                }
+            };
+
+            await saveInteractionUseCase.Execute(botInteraction);
+        }
+    }
+}
